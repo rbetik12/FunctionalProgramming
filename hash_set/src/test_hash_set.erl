69c51f3b275b6d162662beee6db1a698ca35b5d9
Vitaliy <mr.prikota@gmail.com>
2021-11-09 13:29:34 +0300

    Added new unit tests

--- hash_set/src/test_hash_set.erl
+++ hash_set/src/test_hash_set.erl
@@ -22,7 +22,7 @@ del_test() ->
 
 filter_test() ->
   HashSet = hash_set:put(4, hash_set:put(-2, hash_set:put(1, hash_set:new()))),
-  ?assert(hash_set:get_list(hash_set:filter(fun (X) -> X rem 2 == 0 end, HashSet)) == [-2, 4]).
+  ?assert(hash_set:get_list(hash_set:filter(fun(X) -> X rem 2 == 0 end, HashSet)) == [-2, 4]).
 
 map_test() ->
   HashSet = hash_set:put(-3, hash_set:put(3, hash_set:put(-1, hash_set:put(1, hash_set:new())))),
@@ -37,5 +37,44 @@ map_test() ->
 
 fold_test() ->
   HashSet = hash_set:put(3, hash_set:put(-2, hash_set:put(5, hash_set:put(1, hash_set:new())))),
-  ?assert(hash_set:foldl(fun (X, Acc) -> X + Acc end, 0, HashSet) == 7),
-  ?assert(hash_set:foldr(fun (X, Acc) -> X + Acc end, 0, HashSet) == 7).
+  ?assert(hash_set:foldl(fun(X, Acc) -> X + Acc end, 0, HashSet) == 7),
+  ?assert(hash_set:foldr(fun(X, Acc) -> X + Acc end, 0, HashSet) == 7).
+
+%% Let's break some stuff %%
+
+map_then_add_element_test() ->
+  HashSet = hash_set:put(4, hash_set:put(3, hash_set:put(5, hash_set:put(2, hash_set:new())))),
+  MappedHashSet = hash_set:map(
+    fun(X) ->
+      X + 2
+    end,
+    HashSet),
+  ?assert(hash_set:get_list(hash_set:put(6, hash_set:remove(6, MappedHashSet))) == [4, 7, 5, 6]).
+
+filter_atomics_test() ->
+  HashSet = hash_set:put(chebyrek, hash_set:put(lol, hash_set:put(kek, hash_set:new()))),
+  FilteredHashSet = hash_set:filter(
+    fun(X) ->
+      case X of
+        kek -> true;
+        _ -> false
+      end
+    end, HashSet),
+  ?assert(hash_set:get_list(FilteredHashSet) == [kek]).
+
+filter_then_add_element_test() ->
+  HashSet = hash_set:put(chebyrek, hash_set:put(lol, hash_set:put(kek, hash_set:new()))),
+  FilteredHashSet = hash_set:filter(
+    fun(X) ->
+      case X of
+        kek -> true;
+        _ -> false
+      end
+    end, HashSet),
+  ?assert(hash_set:get_list(hash_set:put(lol, FilteredHashSet)) == [kek, lol]).
+
+remove_different_types_test() ->
+  HashSet = hash_set:put("345", hash_set:put(2, hash_set:put(kek, hash_set:new()))),
+  NewHashSet = hash_set:remove("345", HashSet),
+  ?assert(hash_set:get_list(NewHashSet) == [kek, 2]).
+
