f0a63c6a0b0f3d0e1c19e44c41d7b8fbe4c3edda
rbetik12 <mr.prikota@gmail.com>
2021-12-19 14:50:58 +0300

    Added quadratic interpolation

--- lab3/src/function_generator.erl
+++ lab3/src/function_generator.erl
@@ -38,6 +38,33 @@ handle_cast(
     }
   end;
 
+handle_cast(
+    {add_point, X, Y},
+    #generator_state{
+      generator_type = quadratic,
+      points_gen_pid = Pid,
+      points_list = PointList,
+      func_map = FuncMap}) ->
+  PointList1 = lists:append(PointList, [{X, Y}]),
+  case length(PointList1) of
+    3 -> {
+      noreply,
+      #generator_state{
+        generator_type = quadratic,
+        points_list = [lists:nth(3, PointList1)],
+        func_map = generate_function(quadratic, PointList1, FuncMap, Pid),
+        points_gen_pid = Pid}
+    };
+    _ -> {
+      noreply,
+      #generator_state{
+        generator_type = quadratic,
+        points_list = PointList1,
+        func_map = FuncMap,
+        points_gen_pid = Pid}
+    }
+  end;
+
 handle_cast({get_func_map}, #generator_state{func_map = FuncMap} = State) -> {reply, FuncMap, State}.
 
 send_message(Pid, Data) -> gen_server:cast(Pid, Data).
@@ -50,5 +77,16 @@ generate_function(linear, PointsList, FuncMap, Pid) ->
   A1 = (Y2 - Y1) / (X2 - X1),
   A0 = Y1 - A1 * X1,
   Func = fun (X) -> A0 + A1 * X end,
+  points_generator:send_message(Pid, {{X1, X2}, Func}),
+  maps:put({X1, X2}, Func, FuncMap);
+
+generate_function(quadratic, PointsList, FuncMap, Pid) ->
+  {X1, Y1} = lists:nth(1, PointsList),
+  {X2, Y2} = lists:nth(2, PointsList),
+  {X3, Y3} = lists:nth(3, PointsList),
+  A2 = ((Y3 - Y1) / ((X3 - X1) * (X3 - X2))) - ((Y2 - Y1)/((X2 - X1) * (X3 - X2))),
+  A1 = ((Y2 - Y1) / (X2 - X1)) - (A2 * (X2 + X1)),
+  A0 = Y1 - A1 * X1 - A2 * X1 * X1,
+  Func = fun (X) -> A0 + A1 * X + A2 * X * X end,
   points_generator:send_message(Pid, {{X1, X2}, Func}),
   maps:put({X1, X2}, Func, FuncMap).
